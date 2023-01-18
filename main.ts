9fda8f516aad3b17439f2e84e5680dfa52196525
IPedr4o <122974238+IPedr4o@users.noreply.github.com>
2023-01-18 10:21:57 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-let pedra_papel_tesoura = 0
+let pedraPapelTesoura = 0
 input.onGesture(Gesture.Shake, function () {
-    pedra_papel_tesoura = randint(1, 3)
-    if (pedra_papel_tesoura == 1) {
+    pedraPapelTesoura = randint(1, 3)
+    if (pedraPapelTesoura == 1) {
         basic.showLeds(`
             . . . . .
             # # # # #
@@ -9,7 +9,7 @@ input.onGesture(Gesture.Shake, function () {
             . . # # #
             . . . . .
             `)
-    } else if (pedra_papel_tesoura == 2) {
+    } else if (pedraPapelTesoura == 2) {
         basic.showLeds(`
             # # # # #
             # # # # #
@@ -17,21 +17,13 @@ input.onGesture(Gesture.Shake, function () {
             # # # # #
             # # # # #
             `)
-    } else if (pedra_papel_tesoura == 3) {
+    } else {
         basic.showLeds(`
             # # . . #
             . # . # .
             . . # . .
             . # . # .
             # # . . #
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
             `)
     }
 })
